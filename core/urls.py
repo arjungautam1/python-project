f5750ef76524d0ca0bb143f8b8614da6891a2db8
Code With Arjun <codewitharjun@macbookpro.home>
2024-12-11 23:10:02 -0500

    first commit

---
+++ core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='task_list'), name='logout'),
+    path('', views.task_list, name='task_list'),
+    path('tasks/new/', views.task_create, name='task_create'),
+    path('tasks/<int:pk>/edit/', views.task_update, name='task_update'),
+    path('tasks/<int:pk>/delete/', views.task_delete, name='task_delete'),
+]
---
+++ core/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm, TaskForm
+from .models import Task
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('task_list')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'core/signup.html', {'form': form})
+
+def task_list(request):
+    query = request.GET.get('q')
+    tasks = Task.objects.filter(title__icontains=query) if query else Task.objects.all()
+    return render(request, 'core/task_list.html', {'tasks': tasks})
+
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'core/task_form.html', {'form': form})
+
+def task_update(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'core/task_form.html', {'form': form})
+
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'core/task_confirm_delete.html', {'task': task})
+
